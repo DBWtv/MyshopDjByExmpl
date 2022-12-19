c4f6c9b040ad210c6d4e2099ab68e1581963e9d2
DBWtv <dima.bwc@gmail.com>
2022-12-19 16:30:25 +0600

    fix mistake

--- cart/cart.py
+++ cart/cart.py
@@ -55,5 +55,5 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
-        del self.session[settings.CART_SESSIN_ID]
+        del self.session[settings.CART_SESSION_ID]
         self.save()
